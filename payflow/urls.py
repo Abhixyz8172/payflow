e41b24831765a019533f1864ade8d46acb639604
Abhishek Yadav <abhsihek735yadav@email.com>
2026-03-19 13:22:30 +0530

    Add JWT Authentication, Docker, PostgreSQL

--- payflow/urls.py
+++ payflow/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def home(request):
     return render(request, 'index.html')
@@ -25,4 +26,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/wallet/', include('wallet.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
